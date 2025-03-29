925afb28cd33369eb77203508e4aa8e775ac8351
Andrey Nazarenko <skiminok86@gmail.com>
2025-03-29 20:25:09 +0300

    Add Meal Item Screen

---
+++ components/MealItem.jsx
@@ -0,0 +1,10 @@
+import {StyleSheet, Text, View } from "react-native"
+const MealItem = ({ title }) => {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    )
+}
+
+export default MealItem
--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -2,12 +2,18 @@ import { FlatList } from "react-native"
 import { CATEGORIES } from "../data/dummy-data"
 import CategoryGridTile from "../components/CategoryGridTile"
 
-const CategoriesScreen = () => {
+const CategoriesScreen = ({ navigation }) => {
+    const pressHandler = (item) => {
+        navigation.navigate("MealsOverview", { categoryId: item.id })
+    }
+
     return (
         <FlatList
             data={CATEGORIES}
             keyExtractor={(item) => item.id}
-            renderItem={({ item }) => <CategoryGridTile title={item.title} color={item.color} />}
+            renderItem={({ item }) => (
+                <CategoryGridTile title={item.title} color={item.color} onPress={() => pressHandler(item)} />
+            )}
             numColumns={2}
         />
     )
